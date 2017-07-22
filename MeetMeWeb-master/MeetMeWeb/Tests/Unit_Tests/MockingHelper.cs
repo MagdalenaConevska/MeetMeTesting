87fb67bc2ae2982a03aa73e661e0d72b16efd4d1
Magdalena <magdalenaconevska.10@gmail.com>
2017-07-22 15:36:26 +0200

    Tests for UserService.cs finished

---
+++ MeetMeWeb-master/MeetMeWeb/Tests/Unit_Tests/MockingHelper.cs
@@ -0,0 +1,103 @@
+﻿using MeetMeWeb.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MeetMeWeb.Tests.Unit_Tests
+{
+    public static class MockingHelper
+    {
+        #region PopulationExtensionMethods
+
+        public static List<User> PopulateUsers(this List<User> users, int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                users.Add(new User
+                {
+                    Id = number.ToString(),
+                    FirstName = Guid.NewGuid().ToString(),
+                    LastName = Guid.NewGuid().ToString(),
+                    Email = Guid.NewGuid().ToString()+"@gmail.com",
+                    BirthDate = DateTime.UtcNow,
+                    UserName = Guid.NewGuid().ToString()
+                });
+            }
+            return users;
+        }
+
+        public static List<ConnectionNotification> PopulateConnectionNotifications(this List<ConnectionNotification> connectionNotifications, int number)
+        {
+            List<User> users = new List<User>();
+            users.PopulateUsers(2);
+
+            for (int i = 0; i < number; i++)
+            {
+                connectionNotifications.Add(new ConnectionNotification
+                {
+                    ID = Guid.NewGuid(),
+                    Content = Guid.NewGuid().ToString(),
+                    Date = DateTime.UtcNow,
+                    Read = true,
+                    User1 = users.Skip(0).First(),
+                    User2 = users.Skip(1).First()
+                });
+            }
+            return connectionNotifications;
+        }
+        #endregion
+
+        #region CreatingCopiesMethods
+ 
+        public static User CreateUserCopy(User original)
+        {
+            return new User
+            {
+                Id = original.Id,
+                FirstName = original.FirstName,
+                LastName = original.LastName,
+                Email = original.Email,
+                BirthDate = original.BirthDate,
+                UserName = original.UserName
+            };
+        }
+
+        public static ConnectionNotification CreateConnectionNotificationCopy(ConnectionNotification original)
+        {
+            return new ConnectionNotification
+            {
+                ID = original.ID,
+                Content = original.Content,
+                Date = original.Date,
+                Read = original.Read,
+                User1 = CreateUserCopy(original.User1),
+                User2 = CreateUserCopy(original.User2)
+            };
+        }
+        #endregion
+
+        #region CheckAssertsMethods
+
+        public static void CheckAssertsForUser(User expected, User actual)
+        {
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
+            Assert.AreEqual(expected.Email, actual.Email);
+            Assert.AreEqual(expected.UserName, actual.UserName);
+            Assert.AreEqual(expected.BirthDate, actual.BirthDate);
+        }
+
+        public static void CheckAssertsForConnectionNotification(ConnectionNotification expected, ConnectionNotification actual)
+        {
+            Assert.AreEqual(expected.ID, actual.ID);
+            Assert.AreEqual(expected.Content, actual.Content);
+            Assert.AreEqual(expected.Date, actual.Date);
+            CheckAssertsForUser(expected.User1, actual.User1);
+            CheckAssertsForUser(expected.User2, actual.User2);
+        }
+
+        #endregion
+    }
+}
--- MeetMeWeb-master/MeetMeWeb/Tests/Unit_Tests/UserServiceTests.cs
+++ MeetMeWeb-master/MeetMeWeb/Tests/Unit_Tests/UserServiceTests.cs
@@ -3,17 +3,25 @@ using MeetMeWeb.Repositories.Interfaces;
 using NUnit.Framework;
 using MeetMeWeb.Models;
 using MeetMeWeb.Services;
+using System.Linq;
+using System.Collections.Generic;
 using System;
 
 namespace MeetMeWeb.Tests.Unit_Tests
 {
     [TestFixture]
     public class UserServiceTests
-    { 
+    {
         Mock<UserService> _userServiceMock;
-            
+
         Mock<IUserRepository> _userRepositoryMock;
         Mock<IConnectionNotificationRepository> _connectionNotificationRepositoryMock;
+
+        List<User> _users;
+        List<ConnectionNotification> _connectionNotifications;
+
+        List<User> expectedUsers;
+        List<ConnectionNotification> expectedConnectionNotifications;
 
         [SetUp]
         public void Setup()
@@ -21,10 +29,17 @@ namespace MeetMeWeb.Tests.Unit_Tests
             _userRepositoryMock = new Mock<IUserRepository>();
             _connectionNotificationRepositoryMock = new Mock<IConnectionNotificationRepository>();
 
+            _users = new List<User>();
+            _connectionNotifications = new List<ConnectionNotification>();
+
+            expectedUsers = new List<User>();
+            expectedConnectionNotifications = new List<ConnectionNotification>();
+
             _userServiceMock = new Mock<UserService>(new object[] { _userRepositoryMock.Object, _connectionNotificationRepositoryMock.Object })
             {
                 CallBase = true
             };
+
         }
 
         #region getUserById
@@ -32,30 +47,133 @@ namespace MeetMeWeb.Tests.Unit_Tests
         [Test]
         public void getUserById_UserFound()
         {
-            User mockedUser = new User {
-                Id = "TestId",
-                FirstName = "Magdalena",
-                LastName = "Conevska",
-                Email = "magde@gmail.com",
-                BirthDate = DateTime.UtcNow
-            };
+            User mockedUser = _users.PopulateUsers(1).FirstOrDefault();
+            
+            User expected = MockingHelper.CreateUserCopy(mockedUser);
+
             //We use AAA syntax in each unit test in this project
 
             //Arrange
             _userRepositoryMock.Setup(f => f.getUserById(It.IsAny<string>())).Returns(mockedUser);
 
             //Act
-            var result = _userServiceMock.Object.getUserById("TestId");
+            var result = _userServiceMock.Object.getUserById(mockedUser.Id);
 
             //Assert
             _userRepositoryMock.Verify(f => f.getUserById(It.Is<string>(v => v == mockedUser.Id)), Times.Once);
 
-            Assert.AreEqual(mockedUser.Id, result.Id);
-            Assert.AreEqual(mockedUser.FirstName, result.FirstName);
-            Assert.AreEqual(mockedUser.LastName, result.LastName);
-            Assert.AreEqual(mockedUser.Email, result.Email);
-            Assert.AreEqual(mockedUser.BirthDate, result.BirthDate);
+            MockingHelper.CheckAssertsForUser(expected, result);
         }
         #endregion
-    }
+
+        #region getUserByUsername
+
+        [Test]
+        public void getUserByUsername_UserFound()
+        {
+            User mockedUser = _users.PopulateUsers(1).FirstOrDefault();
+
+            User expected = MockingHelper.CreateUserCopy(mockedUser);
+
+            _userRepositoryMock.Setup(f => f.getUserByUsername(It.IsAny<string>())).Returns(mockedUser);
+
+            var result = _userServiceMock.Object.getUserByUsername(mockedUser.UserName);
+
+            _userRepositoryMock.Verify(f => f.getUserByUsername(It.Is<string>(v => v == mockedUser.UserName)), Times.Once);
+
+            MockingHelper.CheckAssertsForUser(expected, result);
+        }
+        #endregion
+
+        #region getAll
+
+        [Test]
+        public void getAll_UsersFound()
+        {
+            _users.PopulateUsers(5);
+
+            foreach (User user in _users)
+            {
+                expectedUsers.Add(MockingHelper.CreateUserCopy(user));
+            }
+
+            _userRepositoryMock.Setup(f => f.getAll()).Returns(_users);
+
+            var result = _userServiceMock.Object.getAll();
+
+            _userRepositoryMock.Verify(f => f.getAll(), Times.Once);
+
+            Assert.AreEqual(expectedUsers.Count, result.Count);
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                MockingHelper.CheckAssertsForUser(expectedUsers.Skip(i).First(), result.Skip(i).First());
+            }
+        }
+        #endregion
+
+        #region getConnectionNotifications
+
+        [Test]
+        public void getConnectionNotifications_ResultListNotEmpty()
+        {
+            _connectionNotifications.PopulateConnectionNotifications(2);
+
+            foreach (ConnectionNotification connectionNotification in _connectionNotifications)
+            {
+                expectedConnectionNotifications.Add(MockingHelper.CreateConnectionNotificationCopy(connectionNotification));
+            }
+
+            _connectionNotificationRepositoryMock.Setup(f => f.getConnectionNotifications(It.IsAny<string>(),
+                                                                                          It.IsAny<int>(),
+                                                                                          It.IsAny<int>())).Returns(_connectionNotifications);
+
+            _connectionNotificationRepositoryMock.Setup(f => f.readConnestionNotification(It.IsAny<ConnectionNotification>()));
+
+            var result = _userServiceMock.Object.getConnectionNotifications(_connectionNotifications.First().User2.Id, 0, _connectionNotifications.Count);
+
+            _connectionNotificationRepositoryMock.Verify(f => f.getConnectionNotifications(It.Is<string>(k => k == _connectionNotifications.First().User2.Id),
+                                                                                           It.Is<int>(k => k == 0),
+                                                                                           It.Is<int>(k => k == _connectionNotifications.Count)), Times.Once);
+
+            for (int i = 0; i < _connectionNotifications.Count; i++)
+            {
+                _connectionNotificationRepositoryMock.Verify(f => f.readConnestionNotification(It.Is<ConnectionNotification>(k => k.User1.Id == _connectionNotifications.Skip(i).First().User1.Id &&
+                                                                                                                                  k.User2.Id == _connectionNotifications.Skip(i).First().User2.Id &&
+                                                                                                                                  k.ID == _connectionNotifications.Skip(i).First().ID &&
+                                                                                                                                  k.Content == _connectionNotifications.Skip(i).First().Content &&
+                                                                                                                                  k.Date == _connectionNotifications.Skip(i).First().Date
+                                                                                                                                )), Times.Once());
+            }
+
+            Assert.AreEqual(expectedConnectionNotifications.Count, result.Count);
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                MockingHelper.CheckAssertsForConnectionNotification(expectedConnectionNotifications.Skip(i).First(), result.Skip(i).First());
+            }
+        }
+
+        [Test]
+        public void getConnectionNotifications_ResultListIsEmpty()
+        {
+            _connectionNotificationRepositoryMock.Setup(f => f.getConnectionNotifications(It.IsAny<string>(),
+                                                                                         It.IsAny<int>(),
+                                                                                         It.IsAny<int>())).Returns(_connectionNotifications);
+
+            _connectionNotificationRepositoryMock.Setup(f => f.readConnestionNotification(It.IsAny<ConnectionNotification>()));
+
+            var result = _userServiceMock.Object.getConnectionNotifications("User2TestId", 0, 2);
+
+            _connectionNotificationRepositoryMock.Verify(f => f.getConnectionNotifications(It.Is<string>(k => k == "User2TestId"),
+                                                                                           It.Is<int>(k => k == 0),
+                                                                                           It.Is<int>(k => k == 2)), Times.Once);
+
+            _connectionNotificationRepositoryMock.Verify(f => f.readConnestionNotification(It.IsAny<ConnectionNotification>()), Times.Never);
+
+            Assert.AreEqual(expectedConnectionNotifications.Count, result.Count);
+        }
+
+            #endregion
+        }
 }
